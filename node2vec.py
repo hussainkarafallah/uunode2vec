fdb15c2f1fe0b6d111a970de3f04ac1ea4f3f7eb
refrantz <renan262001@gmail.com>
2022-10-12 17:46:54 +0200

    constructor for skipgram

--- node2vec.py
+++ node2vec.py
@@ -1,6 +1,6 @@
 from preprocessing import load_and_process
 from walk_generator import WalkGenerator
-from skipgram import SkipGram
+from skipgram import skipgram
 
 def node2vec(
     dim : int,
@@ -13,8 +13,8 @@ def node2vec(
 ):
     graph = load_and_process()
     generator = WalkGenerator(graph , p , q , walk_length , walks_per_node)
-    skip_gram = SkipGram(graph.n , dim , k , lr)
-    skip_gram.train(generator.generate())
+    skip_gram = skipgram(generator.generate(), embeddingSize=10, batch_size=2, windowSize=1, learningRate=0.01, epochs=150000)
+    skip_gram.trainModel()
 
 
 
--- skipgram.py
+++ skipgram.py
@@ -2,107 +2,129 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from walk_generator import WalkGenerator
 
 #this is a pytorch implementation. Code is throughly commented
 
-#should be number of nodes
-vocabSize=19240
-#number of weights/attributes associated with each node
-embeddingSize=10
 
-#function for generating batches
-def randomBatch(skipGrams):
-    randomInputs = []
-    randomLabels = []
+class skipgram:
 
-    #generates a range of random indexes of size batch_size, replace false means generated indexes are unique
-    randomIndex = np.random.choice(range(len(skipGrams)), batch_size, replace=False)
+    walks = []
+    embeddingSize = 10
+    vocabSize = 19240
+    batch_size = vocabSize
+    windowSize = 1
+    learningRate = 0.001
+    epochs = 150000
 
-    #for every randomly generated index, appends target and context to their arrays
-    for i in randomIndex:
-        randomInputs.append(skipGrams[i][0])  # target
-        randomLabels.append(skipGrams[i][1])  # context word
+    #defining the model as a pytorch model
+    class skipgramModel(nn.Module):
 
-    return randomInputs, randomLabels
+        vocabSize = 19240
+        embeddingSize = 10
 
-#generates node pairs between target nodes and their possible contexts
-def generateSkipgram(walk, windowSize=1):
-   skipGrams = []
-   for i in range(windowSize, len(walk) - windowSize):
-       target = walk[i]
-       #change this if changing windowSize
-       context = [walk[i - windowSize], walk[i + windowSize]]
-       for w in context:
-           skipGrams.append([target, w])
+        def __init__(self, vocabSize, embeddingSize):
 
-   return skipGrams
+            super(skipgramModel, self).__init__()
 
-def generateAllSkipgrams(walk_generator, windowSize=1):
-    skipGrams = []
-    for walk in walk_generator:
-        skipGrams.append(generateSkipgram(walk, windowSize))
+            #initial weights, each node has embeddingSize weights associated to it, this later becomes the embedding
+            self.embedding = nn.Embedding(vocabSize, embeddingSize)
 
-#defining the model as a pytorch model
-class skipgramModel(nn.Module):
+            #first layer weights
+            self.W1 = nn.Linear(embeddingSize, embeddingSize, bias=False)
+            #output layer weights
+            self.W2 = nn.Linear(embeddingSize, vocabSize, bias=False)
 
-    def __init__(self):
-
-        super(skipgramModel, self).__init__()
-
-        #initial weights, each node has embeddingSize weights associated to it, this later becomes the embedding
-        self.embedding = nn.Embedding(vocabSize, embeddingSize)
-
-        #first layer weights
-        self.W1 = nn.Linear(embeddingSize, embeddingSize, bias=False)
-        #output layer weights
-        self.W2 = nn.Linear(embeddingSize, vocabSize, bias=False)
-
-    def forward(self, X):
-        #receives input node
-        embeddings = self.embedding(X)
-        #feeds input node into hidden layer, which goes to a relu activation
-        hidden_layer = nn.functional.relu(self.W1(embeddings))
-        #activates the output
-        output_layer = self.W2(hidden_layer)
-        return output_layer
+        def forward(self, X):
+            #receives input node
+            embeddings = self.embedding(X)
+            #feeds input node into hidden layer, which goes to a relu activation
+            hidden_layer = nn.functional.relu(self.W1(embeddings))
+            #activates the output
+            output_layer = self.W2(hidden_layer)
+            return output_layer
 
 
 
-def trainModel(walk_generator, windowSize=1, training_epochs=150000):
+    def __init__(walks, embeddingSize, batch_size, windowSize, learningRate, epochs):
+        #should be number of nodes
+        vocabSize=19240
+        #number of weights/attributes associated with each node
+        embeddingSize=10
+        
+        self.walks = walks
+        self.batch_size = batch_size
+        self.windowSize = windowSize
+        self.learningRate = learningRate
+        self.epochs = epochs
 
-    #instantiates skipgram
-    model = skipgramModel()
-    #loss function
-    criterion = nn.CrossEntropyLoss()
-    #pytorch optimizer
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    #function for generating batches
+    def randomBatch(skipGrams):
+        randomInputs = []
+        randomLabels = []
 
-    #genereates all skipgrams for all walks
-    skipGrams = generateAllSkipgrams(walk_generator, windowSize)
-    #forward and backproprag of the model using generated skipgrams
-    #te quiero demasiado
-    for epoch in tqdm(range(training_epochs), total=len(skipGrams)):
-        #generates random batches
-        input_batch, target_batch = random_batch(skipGrams)
-        #puts random batches in a pytorch longtensor for faster computing
-        input_batch = torch.LongTensor(input_batch)
-        target_batch = torch.LongTensor(target_batch)
+        #generates a range of random indexes of size batch_size, replace false means generated indexes are unique
+        randomIndex = np.random.choice(range(len(skipGrams)), self.batch_size, replace=False)
 
-        #reset gradient
-        optimizer.zero_grad()
+        #for every randomly generated index, appends target and context to their arrays
+        for i in randomIndex:
+            randomInputs.append(skipGrams[i][0])  # target
+            randomLabels.append(skipGrams[i][1])  # context word
 
-        #forward proprag
-        output = model(input_batch)
+        return randomInputs, randomLabels
 
-        #calculate loss
-        loss = criterion(output, target_batch)
+    #generates node pairs between target nodes and their possible contexts
+    def generateSkipgram(self, walk):
+        skipGrams = []
+        for i in range(self.windowSize, len(walk) - self.windowSize):
+            target = walk[i]
+            #change this if changing windowSize
+            context = [walk[i - self.windowSize], walk[i + self.windowSize]]
+            for w in context:
+                skipGrams.append([target, w])
 
-        #show loss every 10000 epochs
-        if (epoch + 1) % 10000 == 0:
-            print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
+        return skipGrams
 
-        #backward proprag
-        loss.backward(retain_graph=True)
+    def generateAllSkipgrams(self):
+        skipGrams = []
+        for walk in self.walks:
+            skipGrams.append(generateSkipgram(walk))
 
-        #applies calculated correction
-        optimizer.step()
+    def trainModel():
+
+        #instantiates skipgram
+        model = skipgramModel(self.vocabSize, self.embeddingSize)
+        #loss function
+        criterion = nn.CrossEntropyLoss()
+        #pytorch optimizer
+        optimizer = optim.Adam(model.parameters(), self.learningRate)
+
+        #genereates all skipgrams for all walks
+        skipGrams = generateAllSkipgrams()
+        #forward and backproprag of the model using generated skipgrams
+        #te quiero demasiado
+        for epoch in tqdm(range(self.training_epochs), total=len(skipGrams)):
+            #generates random batches
+            input_batch, target_batch = random_batch(skipGrams)
+            #puts random batches in a pytorch longtensor for faster computing
+            input_batch = torch.LongTensor(input_batch)
+            target_batch = torch.LongTensor(target_batch)
+
+            #reset gradient
+            optimizer.zero_grad()
+
+            #forward proprag
+            output = model(input_batch)
+
+            #calculate loss
+            loss = criterion(output, target_batch)
+
+            #show loss every 10000 epochs
+            if (epoch + 1) % 10000 == 0:
+                print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
+
+            #backward proprag
+            loss.backward(retain_graph=True)
+
+            #applies calculated correction
+            optimizer.step()
