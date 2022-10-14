5d2f22cde972ea4ca86d445d958d7cae1682f024
refrantz <renan262001@gmail.com>
2022-10-14 16:58:16 +0200

    adjustments to node2vec and main, final version

--- node2vec.py
+++ node2vec.py
@@ -2,19 +2,6 @@ from preprocessing import load_and_process
 from walk_generator import WalkGenerator
 from skipgram import skipgram
 
-def node2vec(
-    dim : int,
-    p : float , 
-    q : float , 
-    walk_length : int , 
-    walks_per_node : int , 
-    k : int , 
-    lr : float
-):
-    pass
-    
-#outside of function so it runs and we can test it
-
 graph = load_and_process()
 print("PASSED PREPROCESS")
 generator = WalkGenerator(graph , p=1, q=1, walk_length=10 , per_node=5)
